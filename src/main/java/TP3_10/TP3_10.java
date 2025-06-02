8f624f9ab5ffd5233affc952e097a08a658f2124
gustavogaudsena <gaudereto.sena@gmail.com>
2025-06-02 19:30:14 -0300

    Exercício 10

---
+++ src/main/java/TP3_10/TP3_10.java
@@ -0,0 +1,169 @@
+package TP3_10;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class TP3_10 {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        int currentId = 2;
+
+        try {
+            String responseDelete = deleteById(currentId);
+            System.out.printf("DELETE: %s", responseDelete);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String getDataById(int id) throws IOException, URISyntaxException {
+        URL url = new URI("https://apichallenges.eviltester.com/sim/entities/" + id).toURL();
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Content-Type", "application/json");
+
+        int status = conn.getResponseCode();
+        InputStreamReader streamReader;
+        System.out.println("GET Status: " + status);
+
+        if (status == HttpURLConnection.HTTP_OK) {
+            streamReader = new InputStreamReader(conn.getInputStream());
+        } else {
+            streamReader = new InputStreamReader(conn.getErrorStream());
+        }
+
+        BufferedReader in = new BufferedReader(streamReader);
+
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine).append("\n");
+        }
+
+        if (status == HttpURLConnection.HTTP_NOT_FOUND) {
+            return String.format("Entidade com ID %d não encontrada (Erro 404).", id);
+        }
+
+        in.close();
+        conn.disconnect();
+
+        return content.toString();
+    }
+
+    public static String postData(String data, int id) throws IOException, URISyntaxException {
+        URL url = new URI("https://apichallenges.eviltester.com/sim/entities/" + id).toURL();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+
+        try (DataOutputStream out = new DataOutputStream(conn.getOutputStream())) {
+            out.writeBytes(data);
+            out.flush();
+        }
+
+        int status = conn.getResponseCode();
+        InputStreamReader streamReader;
+
+        streamReader = new InputStreamReader(conn.getInputStream());
+        BufferedReader in = new BufferedReader(streamReader);
+
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine).append("\n");
+        }
+
+        in.close();
+        conn.disconnect();
+
+        return content.toString();
+    }
+
+    public static String putData(String data, int id) throws IOException, URISyntaxException {
+        URL url = new URI("https://apichallenges.eviltester.com/sim/entities/" + id).toURL();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("PUT");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+
+        try (DataOutputStream out = new DataOutputStream(conn.getOutputStream())) {
+            out.writeBytes(data);
+            out.flush();
+        }
+
+        int status = conn.getResponseCode();
+        InputStreamReader streamReader;
+
+        streamReader = new InputStreamReader(conn.getInputStream());
+        BufferedReader in = new BufferedReader(streamReader);
+
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine).append("\n");
+        }
+
+        in.close();
+        conn.disconnect();
+
+        return content.toString();
+    }
+
+    public static String deleteById(int id) throws IOException, URISyntaxException {
+        URL url = new URI("https://apichallenges.eviltester.com/sim/entities/" + id).toURL();
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("DELETE");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Content-Type", "application/json");
+
+        int status = conn.getResponseCode();
+        InputStreamReader streamReader;
+
+
+        System.out.println("DELETE STATUS: " + status);
+
+
+        if (status == HttpURLConnection.HTTP_NO_CONTENT) {
+            streamReader = new InputStreamReader(conn.getInputStream());
+        } else {
+            streamReader = new InputStreamReader(conn.getErrorStream());
+        }
+        BufferedReader in = new BufferedReader(streamReader);
+
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine).append("\n");
+        }
+
+
+        in.close();
+        conn.disconnect();
+
+        if (status == HttpURLConnection.HTTP_FORBIDDEN || status == HttpURLConnection.HTTP_BAD_METHOD) {
+            return String.format("Entidade não pode ser deletada", id);
+        }
+        return content.toString();
+    }
+
+}
